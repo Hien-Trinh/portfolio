f5710cad57f936a07f1f97e6f89630142fc56c97
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-07-06 15:33:32 +0700

    menu update

--- components/HamburgerMenu.js
+++ components/HamburgerMenu.js
@@ -18,16 +18,16 @@ export default function HamburgerMenu() {
             <div className={`${styles.rightLayer} ${styles.menu} ${isActive ? styles.active : null}`}>
                 <ul className="text-white text-6xl relative top-44 left-[12.5%]">
                     <li className="my-2 py-4">
-                        <button className={`${isActive ? styles.bounceIn : null}`} href="#home">Home</button>
+                        <button className={`${isActive ? styles.bounceIn : null}`} onClick={handleClick} href="#home">Home</button>
                     </li>
                     <li className="my-2 py-4">
-                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn2}` : null}`} href="#about">About</button>
+                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn2}` : null}`} onClick={handleClick} href="#about">About</button>
                     </li>
                     <li className="my-2 py-4">
-                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn3}` : null}`} href="#projects">Projects</button>
+                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn3}` : null}`} onClick={handleClick} href="#projects">Projects</button>
                     </li>
                     <li className="my-2 py-4">
-                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn4}` : null}`} href="#contact">Contact</button>
+                        <button className={`${isActive ? `${styles.bounceIn} ${styles.bounceIn4}` : null}`} onClick={handleClick} href="#contact">Contact</button>
                     </li>
                 </ul>
             </div>
