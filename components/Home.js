4ff9ff8aa76be77778571c8d44e42b3732f2d7eb
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-07-06 23:12:02 +0700

    added mousemove to hero

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,30 @@
+import styles from './Home.module.scss'
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+    const [x, y] = useMouseMove()
+
     return (
-        <section id="home" className="w-screen">
-            <div className="absolute top-80 left-48">
+        <section id="home">
+            <div className={styles.hero} style={{top: -((y - 397.5) / 800) + 20 + "rem", left: -((x - 720) / 600) + 12 + "rem"}}>
                 <p className="text-white text-6xl">well done.</p>
             </div>
         </section>
     )
+}
+
+const useMouseMove = () => {
+    const [coords, setCoords] = useState([0, 0])
+
+    useEffect(() => {
+        const handler = ({ clientX, clientY }) => {
+            setCoords([clientX, clientY]);
+        };
+        window.addEventListener('mousemove', handler);
+        return () => {
+            window.removeEventListener('mousemove', handler);
+        };
+    }, []);
+
+    return coords
 }
