9d4c5e9f21362ef6deb51679cf92da90166755dd
David Trinh <2023.hien.minh.trinh@uwcisak.jp>
2022-07-08 12:48:40 +0700

    changed home page title

--- components/Home.js
+++ components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         <section id="home">
             <div className="container h-screen bg-transparent">
                 <div className={styles.hero} style={{transform: `translate3d(${(x - 720) / 50}px, ${(y - 397.5) / 50}px, 0px)`}}>
-                    <p className="text-white text-6xl">well done.</p>
+                    <p className="text-white text-6xl">get good.</p>
                 </div>
             </div>
         </section>
