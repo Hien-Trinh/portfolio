9bae39f09ec0c26a20e8847b9e9a1f617e35f0cc
David Trinh <hientrinhminh32@gmail.com>
2022-07-31 22:55:46 +0700

    separate page transition into lib

--- layouts/Wrapper.js
+++ layouts/Wrapper.js
@@ -1,33 +1,8 @@
 import styles from "./Wrapper.module.scss"
-import { useEffect, useState, useRef } from "react"
-import useScrollMove from "../lib/UseScrollMove"
-import Scrollbar from "smooth-scrollbar"
-
-function usePrevious(value) {
-    const ref = useRef()
-    useEffect(() => {
-        ref.current = value
-    })
-    return ref.current
-}
+import UseScrollPageTransition from "../lib/UseScrollPageTransition"
 
 export default function Wrapper({ children }) {
-    const scrollY = useScrollMove()
-    const prevScrollY = usePrevious(scrollY)
-
-    useEffect(() => {
-        const element = document.getElementById("wrap")
-        const scrollbar = Scrollbar.get(document.getElementById("wrap"))
-
-        const home = document.getElementById("home")
-        const about = document.getElementById("about")
-
-        if (scrollY - prevScrollY > 0) {
-            scrollbar.scrollIntoView(about)
-        } else if (scrollY - prevScrollY < 0) {
-            scrollbar.scrollIntoView(home)
-        }
-    }, [scrollY])
+    UseScrollPageTransition()
 
     return (
         <div id="wrap" className={styles.wrapper}>
