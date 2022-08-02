351697496608310aa7d5e9f9dbf9d566b47d07a5
David Trinh <hientrinhminh32@gmail.com>
2022-08-02 23:00:20 +0700

    fixed loading animation

--- components/Home.js
+++ components/Home.js
@@ -19,16 +19,16 @@ export default function Home() {
     return (
         <div id="home" className="flex justify-center items-center relative h-screen w-screen bg-transparent">
             <Hero pageNum={pageNum} inView={inView} title={<p className="text-white text-9xl mx-9 my-5" style={{transform: `translate3d(${(x - 720) / 50}px, ${(y - 397.5) / 50}px, 0px)`}}>{title}</p>} />
-            <div className={`${styles.art} ${styles.chips} ${inView ? styles.active : null}`}>
+            <div className={styles.art}>
                 <Image src={chips} alt="chips" layout="intrinsic" width={500} height={300} style={{transform: `translate3d(${(x - 720) / 24}px, ${(y - 397.5) / 24}px, 0px)`,position: "absolute", top: 0, right: 0}}/>
             </div>
-            <div className={`${styles.art} ${styles.cola} ${inView ? styles.active : null}`}>
+            <div className={styles.art}>
                 <Image src={cola} alt="cola" layout="intrinsic" width={500} height={300} style={{transform: `translate3d(${(x - 720) / 25}px, ${(y - 397.5) / 25}px, 0px)`,position: "absolute", top: 0, right: 0}}/>
             </div>
-            <div className={`${styles.art} ${styles.donut} ${inView ? styles.active : null}`}>
+            <div className={styles.art}>
                 <Image src={donut} alt="donut" layout="intrinsic" width={500} height={300} style={{transform: `translate3d(${(x - 720) / 22}px, ${(y - 397.5) / 22}px, 0px)`,position: "absolute", top: 0, right: 0}}/>
             </div>
-            <div className={`${styles.art} ${styles.astronaut} ${inView ? styles.active : null}`}>
+            <div className={styles.art}>
                 <Image src={astronaut} alt="astronaut" layout="intrinsic" width={500} height={300} style={{transform: `translate3d(${(x - 720) / 15}px, ${(y - 397.5) / 15}px, 0px)`, position: "absolute", top: 0, right: 0}}/>
             </div>
             <div className={`${styles.license} ${inView? styles.active : null} absolute w-fit right-0 bottom-0 m-2 italic text-sm text-slate-600`}>*vecteezy.com</div>
